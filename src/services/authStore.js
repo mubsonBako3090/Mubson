fc4a8806697b8b138668cf7e4429a58164ace945
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-08-10 03:47:02 +0100

    Delete src/services/authStore.js

--- src/services/authStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useAuthStore = create((set) => ({
-  user: null, // { id, fullName, email, role, collegeId, facultyId, department }
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null }),
-}));
