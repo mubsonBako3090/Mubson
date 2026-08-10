c90ce555116f78ec2aaf2c72b9f7dd939b6f1492
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-08-10 03:47:17 +0100

    Delete src/services/uiStore.js

--- src/services/uiStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const useUiStore = create((set) => ({
-  sidebarOpen: true,
-  toggleSidebar: () => set((state) => ({ sidebarOpen: !state.sidebarOpen })),
-}));
